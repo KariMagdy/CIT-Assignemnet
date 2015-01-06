bfe755db7f6057db4b8217e47321a3cf8a74fcd7
karim magdy <karim.magdy15@yahoo.com>
2015-01-06 13:18:33 +0200

    working on rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do 
+		@user = FactoryGirl.create :user, :email => "example@example.com", :password=>"00000000"
+		sign_in @user
+	end
+	
+	it "should follow" do
+		@followed = FactoryGirl.create :user, :email => "followed@example.com", :password=>"00000000"
+		follow :followed_id => @followed.id
+		expect(@user.followeds.count).to be > 0
+	end
 end
